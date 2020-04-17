31a01656abc18486aec7a99743a08162e5f6e9fc
Choreman <734399815@qq.com>
2020-04-17 10:48:03 +0800

    修改关联关系

--- sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/hotgoods/entity/HotGoods.java
+++ sc-provider/sc-xzsd-pc/src/main/java/com/xzsd/pc/hotgoods/entity/HotGoods.java
@@ -30,18 +30,55 @@ public class HotGoods extends BaseEntity {
     //-----------------关联关系-----------------
 
     /**
-     * 一个热门位商品关联一件商品，一对一
+     * 用于展示商品的编号（年月日时分秒+2位随机数）
      */
-    private Goods goods;
+    private String goodsCode;
+    /**
+     * 商品的名称
+     */
+    private String goodsName;
+    /**
+     * 商品的销售价格
+     */
+    private Float goodsSalePrice;
+    /**
+     * 商品的详细介绍
+     */
+    private String goodsDescription;
 
     //-----------------get和set方法-----------------
 
-    public Goods getGoods() {
-        return goods;
+
+    public String getGoodsCode() {
+        return goodsCode;
     }
 
-    public void setGoods(Goods goods) {
-        this.goods = goods;
+    public void setGoodsCode(String goodsCode) {
+        this.goodsCode = goodsCode;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public Float getGoodsSalePrice() {
+        return goodsSalePrice;
+    }
+
+    public void setGoodsSalePrice(Float goodsSalePrice) {
+        this.goodsSalePrice = goodsSalePrice;
+    }
+
+    public String getGoodsDescription() {
+        return goodsDescription;
+    }
+
+    public void setGoodsDescription(String goodsDescription) {
+        this.goodsDescription = goodsDescription;
     }
 
     public String getHotGoodsId() {
